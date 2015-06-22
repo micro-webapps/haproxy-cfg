090efc77765c2c822d777dfea9a9200d6c2be6c1
Jan Kaluza <jkaluza@redhat.com>
2015-06-22 09:45:50 +0200

    Use haproxy to really test the proxying is working as expected

---
+++ tests/003/requests.sh
@@ -0,0 +1,6 @@
+source ../functions.sh
+
+handled_by_8080 "localhost" "https://localhost:9443/owncloud"
+handled_by_8080 "localhost" "https://localhost:9443/owncloud/"
+handled_by_8080 "localhost" "https://localhost:9443/owncloud/another"
+
--- tests/run.py
+++ tests/run.py
@@ -2,6 +2,39 @@
 
 import sys
 import os
+import socket
+import threading
+try:
+    import SocketServer
+except:
+    import socketserver
+    SocketServer = socketserver
+
+
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        port = str(self.server.server_address[1])
+        data = self.request.recv(8192)
+        f = open(port + ".txt", "w")
+        f.write(str(data))
+        f.close()
+        self.request.sendall(bytes("""HTTP/1.1 200 OK
+Content-Type: application/xhtml; charset=utf-8
+Content-Length: {0}
+
+{1}
+""".format(len(port), port), 'UTF-8'))
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+def start_server(port):
+    SocketServer.TCPServer.allow_reuse_address = True
+    handler = ThreadedTCPRequestHandler
+    server = ThreadedTCPServer(("localhost", port), handler)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.start()
+    return server, server_thread
 
 class bcolors:
     HEADER = '\033[95m'
@@ -13,29 +46,71 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def runtest(d):
-    print(bcolors.OKGREEN + "*** " + d + ": STARTING" + bcolors.ENDC)
+def cleanup(d):
     ret = os.system("rm -f ./" + d + "/*.conf")
+    ret = os.system("rm -f ./" + d + "/8080.txt")
+    ret = os.system("rm -f ./" + d + "/8081.txt")
     ret = os.system("rm -rf ./" + d + "/certs")
+    ret = os.system("rm -rf ./" + d + "/haproxy.output")
 
+def test_generate_cfg(d):
     cmd = 'export `cat {0}`; ../haproxy-cfg {1} {2} --debug; diff -u {3}/webconf.result {4}/haproxy.conf'.format(d + "/test.env", d, d + "/haproxy.conf", d, d)
-    #print("Executing:", cmd)
     ret = os.system(cmd)
     if ret != 0:
         print(bcolors.FAIL + "*** " + d + ": FAILED" + bcolors.ENDC)
         if len(sys.argv) == 2:
             del sys.argv[1]
             os.system("mv {0}/*.conf {1}/webconf.result ".format(d, d))
-        else:
-            sys.exit(1)
+
+    return ret
+
+def test_do_requests(d):
+    server_8080, thread_8080 = start_server(8080)
+    server_8081, thread_8081 = start_server(8081)
+
+    os.system("killall haproxy >/dev/null 2>&1")
+    os.system("haproxy -f " + d + "/haproxy.conf > ./" + d + "/haproxy.output 2>&1 &")
+    os.system("sleep 1")
+
+    os.chdir(d)
+    ret = os.system("/bin/bash ./requests.sh")
+    os.chdir("..")
+    if ret != 0:
+        print(bcolors.FAIL + "*** " + d + ": FAILED" + bcolors.ENDC)
+        print("requests.sh returned an error")
     else:
         print(bcolors.OKGREEN + "*** " + d + ": PASSED" + bcolors.ENDC)
-        ret = os.system("rm -f ./" + d + "/*.conf")
-        ret = os.system("rm -rf ./" + d + "/certs")
+    os.system("killall haproxy")
+    os.system("sleep 1")
+
+    server_8080.shutdown()
+    server_8081.shutdown()
+
+    return ret
+
+def runtest(d):
+    print(bcolors.OKGREEN + "*** " + d + ": STARTING" + bcolors.ENDC)
+    ret = 0
+
+    cleanup(d)
+    
+    tests = []
+    tests.append(test_generate_cfg)
+    tests.append(test_do_requests)
+
+    for test in tests:
+        ret = test(d)
+        if ret != 0:
+            break
+
+    if ret == 0:
+        cleanup(d)
+    return ret
 
 tests = os.listdir(".")
 tests.sort()
 for d in tests:
     if os.path.isdir(d):
-        runtest(d)
+        if runtest(d) != 0:
+            sys.exit(1)
     
